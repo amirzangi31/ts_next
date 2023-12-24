65528739c377051bdd4276e3faec08f361e17fec
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2023-12-24 21:07:03 +0330

    add fonts and change tsconfig for alias

---
+++ src/utils/localFonts.ts
@@ -0,0 +1,103 @@
+import localFont from "next/font/local";
+
+const iranSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum)_UltraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum)_UltraLight.woff",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum)_UltraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum)_Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum)_Light.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum)_Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum)_Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum)_Medium.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum)_Medium.ttf",
+      weight: "400",
+      style: "normal",
+    },
+
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum).woff2",
+      weight: "normal",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum).woff",
+      weight: "normal",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum).ttf",
+      weight: "normal",
+      style: "normal",
+    },
+
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum)_Bold.woff2",
+      weight: "bold",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum)_Bold.woff",
+      weight: "bold",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum)_Bold.ttf",
+      weight: "bold",
+      style: "normal",
+    },
+
+    {
+      path: "../../public/fonts/woff2/IRANSansWeb(FaNum)_Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff/IRANSansWeb(FaNum)_Black.woff",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ttf/IRANSansWeb(FaNum)_Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
+
+export default iranSans;
