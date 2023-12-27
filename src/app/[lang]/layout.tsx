802a925737e33582342213b05f44df2435931488
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2023-12-27 18:41:51 +0330

    add global css

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import type { Metadata } from 'next'
 import './globals.css'
 import iranSans from '@utils/localFonts'
+import { Locale, i18n } from '@root/i18n.config'
 
-
+export async function generateStaticParams() {
+  return i18n.locales.map(locale => ({ lang: locale }))
+}
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,9 +15,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  params: { lang: Locale }
 }) {
+
+
   return (
     <html lang="en">
       <body className={iranSans.className}>{children}</body>
---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className='text-[0.75rem]'>امیرمحمد زنگی ابادی</h1>
+  )
+}
