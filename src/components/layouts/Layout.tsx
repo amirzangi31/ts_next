c78e87040710658f2edc0b6e7035eaedb3779931
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2024-01-12 12:58:11 +0330

    add react-query to project

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,16 +1,25 @@
-import { ReactNode } from 'react'
+"use client"
+
+import { ReactNode, useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
 
 const Layout = ({ children }: { children: ReactNode }) => {
+    const [query] = useState(() => new QueryClient())
     return (
-        <>
+        <QueryClientProvider client={query}>
             <Header />
             <main className='container py-4 min-h-[calc(100vh-26.5375rem)]'>
                 {children}
             </main>
             <Footer />
-        </>
+            <ReactQueryDevtools initialIsOpen={false } />
+        </QueryClientProvider>
     )
 }
 
