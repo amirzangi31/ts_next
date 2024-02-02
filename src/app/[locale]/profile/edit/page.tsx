3d03e476f951b1876c514c11a98da79beba298f8
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2024-02-02 21:14:22 +0330

    add base edit page

---
+++ src/app/[locale]/profile/edit/page.tsx
@@ -0,0 +1,10 @@
+import EditProfilePage from '@templates/profile/EditProfilePage'
+import React from 'react'
+
+const EditProfile = () => {
+  return (
+    <EditProfilePage /> 
+  )
+}
+
+export default EditProfile
