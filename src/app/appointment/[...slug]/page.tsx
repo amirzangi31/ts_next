e8b9fcec2cf379df844e7768a0b29fe4bb84a437
amir zangiabadi <zangiabadi1378888@gmail.com>
2024-02-13 12:09:19 +0330

    add appointment title page

--- src/app/appointment/[...slug]/page.tsx
+++ src/app/appointment/[...slug]/page.tsx
@@ -8,6 +8,41 @@ import convertToHour from '@/utils/convertHour'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
+export async function generateMetadata({ params }: {
+    params: {
+        slug: string[],
+        locale: string
+    }
+}) {
+    if (!params.slug[1]) {
+      redirect("/404")
+    }
+  
+    let physicianCalendar;
+  
+    try {
+        const res = await fetch(`${apiDomainNobat}${urls.appointment.calendarPhysician.url}${params.slug[1]}`, {
+            cache: "no-store"
+        })
+        const data = await res.json()
+        physicianCalendar = data.value
+    } catch (error) {
+        redirect("/404")
+    }
+    
+    
+    const title = `نوبت دهی دکتر ${physicianCalendar?.physicianProfile?.firstName} ${physicianCalendar?.physicianProfile?.lastName}`
+    const description = `${physicianCalendar?.physicianProfile?.firstName}`
+  
+  
+  
+    return {
+      title,
+      description,
+      authors: [{ name: title }]
+    }
+  }
+
 const Appointment = async ({ params }: {
     params: {
         slug: string[],
