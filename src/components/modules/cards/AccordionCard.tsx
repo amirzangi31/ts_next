b8067688c81e5edb1026de80a19574f3d8cd7653
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2024-02-14 13:56:44 +0330

    add hover faq section

--- src/components/modules/cards/AccordionCard.tsx
+++ src/components/modules/cards/AccordionCard.tsx
@@ -6,23 +6,27 @@ export type AccordionCardType = {
       title: string,
       description: string,
       active: number | null,
-      handler: (id : number) => void,
+      handler: (id: number) => void,
       id: number
 }
 
 
-const AccordionCard = ({ title, description, active, handler, id } : AccordionCardType) => {
+const AccordionCard = ({ title, description, active, handler, id }: AccordionCardType) => {
       return (
             <div
-                  className={`bg-white  p-5 shadow-shadow_category relative rounded-sm w-full cursor-pointer hover:bg-primary
-                        transition-all duration-300 group
+                  className={`bg-white  p-5 shadow-shadow_category relative rounded-sm w-full cursor-pointer overflow-hidden 
+                        transition-all duration-300 group hover:relative
                   `}
                   onClick={() => handler(id)}
             >
-                  <div className="text-lg flex justify-between items-center font-bold relative after:absolute after:rtl:-right-[1.25rem] after:rounded-lg after:top-0 after:block after:bg-primary after:w-1.5 after:h-full">
-                        <h2 className="group-hover:text-white transition-all duration-300">{title}</h2>
+                  <div className={cn(
+                        "text-lg flex justify-between items-center font-bold relative group-hover:static ",
+                        "after:absolute after:rtl:-right-[1.25rem] after:rounded-sm after:transition-all after:duration-500 after:top-0 after:block after:bg-primary after:w-1.5 after:h-full",
+                        "group-hover:after:bg-primary group-hover:after:h-[calc(100%+2.5rem)] group-hover:after:-top-[1.25rem] group-hover:after:w-[calc(100%+1.25rem)] group-hover:after:-z-1"
+                  )}>
+                        <h2 className="group-hover:text-white transition-all duration-300 z-[1]">{title}</h2>
                         <span
-                              className={cn(` transition-all duration-500`, {
+                              className={cn(` transition-all duration-500 delay-700 z-[1]`, {
                                     "rotate-90": active === id,
                               })}
                         >
