ae74637f56539c4bbaa6e059687dbdd4ae24bf70
amir zangiabadi <zangiabadi1378888@gmail.com>
2024-02-22 15:48:50 +0330

    add page

--- src/components/templates/PhysiciansCityPage.tsx
+++ src/components/templates/PhysiciansCityPage.tsx
@@ -4,6 +4,8 @@ import TitlePagesMobile from '../modules/titles/TitlePagesMobile'
 import useCity from '@/hooks/useCity'
 import LinkElement from '../elements/LinkElement'
 import Image from 'next/image'
+import TitleHeading from '../modules/titles/TitleHeading'
+import ArrowLeft from '../icons/ArrowLeft'
 
 const PhysiciansCityPage = ({ data, city }: {
     data: {
@@ -49,8 +51,23 @@ const PhysiciansCityPage = ({ data, city }: {
 
 
             {/* ----------header------------- */}
-            
+            <header className='py-4'>
+                <TitleHeading title={`دکترهای شهر ${provinces.find((item: {
+                    cityId: number,
+                    cityName: string,
+                    centerName: string,
+                    provinceId: number,
+                    provinceName: string,
+                    cityEnName: string
+                }) => item.cityEnName === city)?.cityName}`} />
+            </header>
             {/* ----------header------------- */}
+
+            {/* ----------main------------- */}
+            <main className='mt-4 grid grid-cols-1 md:grid-cols-2 gap-2'>
+                {data.map((item, index) => <LinkCard key={item.specialtyId} link={`physicians/specialty/${item.specialtyEnName}`} title={item.specialtyName} count={item.physicianCount} />)}
+            </main>
+            {/* ----------main------------- */}
 
 
         </>
@@ -58,3 +75,17 @@ const PhysiciansCityPage = ({ data, city }: {
 }
 
 export default PhysiciansCityPage
+
+
+const LinkCard = (props: { link: string, title: string, count: number }) => {
+    const { provinces } = useCity()
+
+    return (
+        <LinkElement link={props.link} className='flex justify-between items-center gap-2 rounded-sm shadow-shadow_category p-5 bg-white'>
+            <Image src={"/favicon.png"} width={500} height={500} alt='icon' className='size-[1.5rem]' />
+            <p className='font-bold text-primary'>{props.title}</p>
+            <p className='flex justify-start items-center gap-2'><span className='font-bold'>{props.count}</span> دکتر <ArrowLeft /> </p>
+            
+        </LinkElement>
+    )
+}
