dca31d9d6fa0cf9b7ddce1c1989977b493986b49
amir zangiabadi <zangiabadi1378888@gmail.com>
2024-02-29 10:16:05 +0330

    ddd

--- src/components/modules/search/SpecialityTagSearch.tsx
+++ src/components/modules/search/SpecialityTagSearch.tsx
@@ -3,9 +3,10 @@ import { SpecialitySearchTagType } from '@/types/search'
 import React from 'react'
 
 const SpecialityTagSearch = (props: SpecialitySearchTagType) => {
-      const { name } = props
+      const { name , enName } = props
+      
       return (
-            <LinkElement link={`physicians/specialty/${name}`} className='border border-gray-300 bg-white rounded-3xl flex justify-center items-center text-md h-[2.8125rem] transition-all duration-300 px-4 hover:bg-gray-300 hover:text-white '>
+            <LinkElement link={`physicians/specialty/${enName}`} className='border border-gray-300 bg-white rounded-3xl flex justify-center items-center text-md h-[2.8125rem] transition-all duration-300 px-4 hover:bg-gray-300 hover:text-white '>
                   {name}
             </LinkElement>
       )
