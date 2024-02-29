8b3cc5ef8fcca8bdf1297e9cffcb7b0b0cf218dc
amir zangiabadi <zangiabadi1378888@gmail.com>
2024-02-29 09:58:32 +0330

    change link specialty tag

--- src/components/modules/search/SpecialityTagSearch.tsx
+++ src/components/modules/search/SpecialityTagSearch.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const SpecialityTagSearch = (props: SpecialitySearchTagType) => {
       const { name } = props
       return (
-            <LinkElement link={`search?speciality=${name}`} className='border border-gray-300 bg-white rounded-3xl flex justify-center items-center text-md h-[2.8125rem] transition-all duration-300 px-4 hover:bg-gray-300 hover:text-white '>
+            <LinkElement link={`physicians/specialty/${name}`} className='border border-gray-300 bg-white rounded-3xl flex justify-center items-center text-md h-[2.8125rem] transition-all duration-300 px-4 hover:bg-gray-300 hover:text-white '>
                   {name}
             </LinkElement>
       )
